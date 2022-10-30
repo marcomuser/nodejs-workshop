653856a63459c13a70a5912340acb953977b1093
Marco Muser <marco.muser@posteo.de>
2022-10-30 16:24:29 +0100

    add sort into folders script

---
+++ sort-into-folders.mjs
@@ -0,0 +1,57 @@
+import fs from "fs/promises";
+
+const photosFolderPath = "./photos";
+const sortedFolderPath = "./sorted";
+
+// get an array with all file names of our photos
+const fileNames = await fs.readdir(photosFolderPath);
+
+// lookup object to match the correct name of the month
+const months = {
+  "01": "January",
+  "02": "Feburary",
+  "03": "March",
+  "04": "April",
+  "06": "June",
+  "05": "May",
+  "07": "July",
+  "08": "August",
+  "09": "September",
+  10: "October",
+  11: "November",
+  12: "December",
+};
+
+// Check if we already have a sorted directory. IF not, create one.
+try {
+  await fs.access(sortedFolderPath);
+} catch {
+  await fs.mkdir(sortedFolderPath);
+}
+
+// loop over all file names
+for (const fileName of fileNames) {
+  // extract the date from the file name
+  const date = fileName
+    .split("_")
+    .filter((substring) => substring.length === 8)
+    .join();
+
+  // extract the year and the month from the date string
+  const year = date.slice(0, 4);
+  const month = date.slice(4, 6);
+
+  const dateFolderName = year + " " + months[month];
+
+  // Check if we already have a folder for the date of the photo. If not, create one.
+  try {
+    await fs.access(sortedFolderPath + "/" + dateFolderName);
+  } catch {
+    await fs.mkdir(sortedFolderPath + "/" + dateFolderName);
+  }
+
+  await fs.rename(
+    photosFolderPath + "/" + fileName,
+    sortedFolderPath + "/" + dateFolderName + "/" + fileName
+  );
+}
